9c1f65fa06fb48e003f0e2444633c0c189ed025e
dani <eleazar48a@gmail.com>
2018-11-06 01:08:30 -0600

    servidor express y conexión con mongo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import express from "express";
 
-console.log("holi");
+const app = express();
+
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/graphql-test')
+    .then ( () => console.log('Conexión a mongo establecida :)'))
+    .catch(err => console.log(`Error al conectar a mongo ): ${err}`));
+    
+
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log('holi :)');
+})
